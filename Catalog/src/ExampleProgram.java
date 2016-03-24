c5820c2f7d7fc43b22d118ca6897eb0fbcb2db5b
niranjan <bnrbangalore21@gmail.com>
2016-03-24 12:13:04 +0530

    Added all the files to flipkart

---
+++ Catalog/src/ExampleProgram.java
@@ -0,0 +1,11 @@
+class ExampleProgram {
+  public static void main(String[] args){
+    System.out.println("Simple Java source code to demonstrate\ncommandline or manual compilation process");
+    System.out.println("Simple message");
+    System.out.println("Simple message");
+  }
+  public void TalkToRobo()
+  {
+    System.out.println("Hello");
+  }
+}
